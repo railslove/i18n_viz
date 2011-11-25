50d42526ecf501acbf9555e5594fc6120f6f3008
Jakob Hilden <jakobhilden@gmail.com>
2011-11-25 13:31:34 +0100

    added helper method display_i18n_viz? and some other improvements

--- lib/i18n_viz/view_helpers.rb
+++ lib/i18n_viz/view_helpers.rb
@@ -1,23 +1,26 @@
 module I18nViz
   module ViewHelpers
-    #module InstanceMethods
-      
-      def t(key, options = {}) # TODO: alias
-        if I18nViz.enabled? && options[:i18n_viz] != false
-          # TODO: only show i18n_viz if params[:i18n_viz] == true
-          "#{super(key, options)}--#{key}--"
-        else
-          super(key, options)
-        end
+
+    def translate(key, options = {}) # TODO: alias
+      if display_i18n_viz? && options[:i18n_viz] != false
+        "#{super(key, options)}--#{key}--"
+      else
+        super(key, options)
       end
+    end
+    alias :t :translate
+    
+    def i18n_viz_include_tag # TODO: doesn't work yet
+      return unless display_i18n_viz?
       
-      def i18n_viz_include_tag # TODO: doesn't work yet
-        return unless I18nViz.enabled?
-        
-        stylesheet_link_tag "i18n_viz"
-        javascript_tag "var i18n_viz_key_url = '#{I18nViz.key_url}';"
-        javascript_include_tag "i18n_viz"
-      end
-    #end
+      stylesheet_link_tag("i18n_viz")
+      javascript_tag "var i18n_viz_key_url = '#{I18nViz.key_url}';"
+      javascript_include_tag "i18n_viz"
+    end
+
+    def display_i18n_viz?
+      I18nViz.enabled? && params[:i18n_viz]
+    end
+
   end
 end
