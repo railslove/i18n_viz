145a1e0fb039314c832022e68f239a25ee0a8a3e
Jakob Hilden <jakobhilden@gmail.com>
2012-03-12 23:02:50 +0100

    more tests

--- test/i18n_viz_test.rb
+++ test/i18n_viz_test.rb
@@ -4,17 +4,28 @@ require 'test_helper'
 class I18nVizIntegrationTest < ActionDispatch::IntegrationTest
   test 'translate without i18n_viz url parameter' do
     visit "/test"
-    
+
     assert !page.has_content?("--hello--")
     assert !page.has_css?("#i18n_viz_tooltip")
   end
 
   test 'translate with i18n_viz url parameter' do
+    I18nViz.enabled = true
     visit "/test?i18n_viz=1"
 
     assert page.has_content?("--hello--")
     assert !page.has_css?(".i18n-viz")
     assert !page.has_css?("#i18n_viz_tooltip")
+
+    assert page.has_content?("bar")
+    assert !page.has_content?("--foo--")
+  end
+
+  test 'disable I18nViz' do
+    I18nViz.enabled = false
+    visit "/test?i18n_viz=1"
+
+    assert !page.has_content?("--hello--")
   end
 end
 
