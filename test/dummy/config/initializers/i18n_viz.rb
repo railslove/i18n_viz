bc22814ab0ad196907005db2ece27db588349836
Jakob Hilden <jakobhilden@gmail.com>
2012-04-19 15:08:07 +0200

    added a basic example initializer, to enable i18n_viz only in non production environments.

---
+++ test/dummy/config/initializers/i18n_viz.rb
@@ -0,0 +1,5 @@
+# encoding: utf-8
+if defined?(I18nViz)
+  I18nViz.enabled            = !Rails.env.production?
+  #I18nViz.external_tool_url = "https://webtranslateit.com/en/projects/xxx/locales/en..de/strings?utf8=✓&s=" # link to more information about each i18n key, e.g. "http://mytranslationtool.com/?key=" -- the key (e.g. 'en.foo.bar') will be appended to this URL
+end
