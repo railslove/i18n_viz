35f8b897cc1d8ee21d3c819262db8be2f0bad8f5
Jakob Hilden <jakobhilden@gmail.com>
2012-05-08 23:56:21 +0200

    check for the environment directly in the asset files instead of using I18nViz.enabled? which didn't work in all cases.

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -58,6 +58,7 @@ module Dummy
     config.assets.compress = false
     config.assets.debug = true
     config.assets.enabled = true
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
