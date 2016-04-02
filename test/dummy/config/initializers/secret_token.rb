cb4666538014367969b5215b08bd10da4117e52f
Jakob Hilden <jakobhilden@gmail.com>
2016-04-02 12:36:38 -0700

    fix rails dummpy app deprecations warnings during test runs

--- test/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = 'f663201ec4caeb717b93ac5f49cbf722d9026f134688c6c7452abf773db92a75067eb4df824124115ecc13f041a43be432590b8aa193ca580f755e4a25ea28a8'
